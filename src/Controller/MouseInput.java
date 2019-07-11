9c56f34f42d8a7225611bbe2c10de63a40202bb2
Balazs <Hungary3176>
2019-07-11 16:46:11 +0300

    Sounds

--- src/Controller/MouseInput.java
+++ src/Controller/MouseInput.java
@@ -4,10 +4,12 @@ import Model.*;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class MouseInput extends MouseAdapter {
 
     private Mediator mediator;
+    private Random r = new Random();
 
     public MouseInput(Mediator mediator) {
         this.mediator = mediator;
@@ -25,6 +27,39 @@ public class MouseInput extends MouseAdapter {
         if(mediator.objectsContainer.getReflectableBullet() != null &&
                 mediator.isShootable(player.getX(),player.getY(), bullet.getX(), bullet.getY())
                 && bullet.bulletType == BulletType.ENEMY){
+
+            int i = r.nextInt(9);
+            switch (i+1)
+            {
+                case 1:
+                    SoundContainer.PANDA1.play();
+                    break;
+                case 2:
+                    SoundContainer.PANDA2.play();
+                    break;
+                case 3:
+                    SoundContainer.PANDA3.play();
+                    break;
+                case 4:
+                    SoundContainer.PANDA4.play();
+                    break;
+                case 5:
+                    SoundContainer.PANDA5.play();
+                    break;
+                case 6:
+                    SoundContainer.PANDA6.play();
+                    break;
+                case 7:
+                    SoundContainer.PANDA7.play();
+                    break;
+                case 8:
+                    SoundContainer.PANDA8.play();
+                    break;
+                case 9:
+                    SoundContainer.PANDA9.play();
+                    break;
+
+            }
 
             mediator.factory.newFriendlyBullet(bullet.getX(),bullet.getY(),mx,my);
             mediator.objectsContainer.removeBullet(bullet);
--- src/Model/SoundContainer.java
+++ src/Model/SoundContainer.java
@@ -7,7 +7,16 @@ import javax.sound.sampled.*;
 public enum SoundContainer {
     THEME("/res/theme_song.wav"),
     INJURED("/res/injured_sound.wav"),
-    SQUISH("/res/squish_sound.wav");
+    SQUISH("/res/squish_sound.wav"),
+    PANDA1("/res/panda_sound_1.wav"),
+    PANDA2("/res/panda_sound_2.wav"),
+    PANDA3("/res/panda_sound_3.wav"),
+    PANDA4("/res/panda_sound_4.wav"),
+    PANDA5("/res/panda_sound_5.wav"),
+    PANDA6("/res/panda_sound_6.wav"),
+    PANDA7("/res/panda_sound_7.wav"),
+    PANDA8("/res/panda_sound_8.wav"),
+    PANDA9("/res/panda_sound_9.wav");
 
     public static enum Volume {
         MUTE, LOW, MEDIUM, HIGH
