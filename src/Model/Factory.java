0da984ee48e14e94406c122d83fc12b49259d963
omerag <omerag@my.hit.ac.il>
2019-07-12 19:30:02 +0300

    omer

--- src/Model/Factory.java
+++ src/Model/Factory.java
@@ -7,7 +7,6 @@ public class Factory {
 
     private Mediator mediator;
     private Game game;
-    private SpriteSheet spriteSheet;
     private ObjectsContainer objectsContainer;
     private int gameWidth;
     private int gameHeight;
@@ -16,46 +15,41 @@ public class Factory {
                    Mediator mediator, SpriteSheet spriteSheet){
         this.mediator = mediator;
         this.game = game;
-        this.spriteSheet = spriteSheet;
         objectsContainer = ObjectsContainer.getInstance();
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
     }
 
-    public Player newPlayer(int x, int y){
+
+
+    public void newPlayer(int x, int y){
         Player player = new Player(x,y,ID.Player, mediator,game,gameWidth, gameHeight);
         objectsContainer.addPlayer(player);
-        return player;
     }
 
-    public Block newBlock(int x, int y){
+    public void newBlock(int x, int y){
         Block block = new Block(x,y,ID.Block,gameWidth,gameHeight);
         objectsContainer.addBlock(block);
-        return block;
     }
 
-    public EnemySpawner newEnemySpawmer(int x, int y){
+    public void newEnemySpawmer(int x, int y){
         EnemySpawner enemySpawner = new EnemySpawner(x,y,ID.Enemy, mediator,game,gameWidth,gameHeight);
         objectsContainer.addEnemySpawner(enemySpawner);
-        return enemySpawner;
     }
 
-    public Enemy newEnemy(int x, int y){
+    public void newEnemy(int x, int y){
         Enemy enemy = new Enemy(x,y, ID.Enemy, mediator,game,gameWidth,gameHeight);
         objectsContainer.addEnemy(enemy);
-        return enemy;
     }
 
-    public Bullet newEnemyBullet(int x, int y,int mx, int my){
+    public void newEnemyBullet(int x, int y,int mx, int my){
         Bullet bullet = new Bullet(x,y,ID.Bullet, mediator,mx, my,BulletType.ENEMY,gameWidth,gameHeight);
         objectsContainer.addBullet(bullet);
-        return bullet;
     }
 
-    public Bullet newFriendlyBullet(int x, int y, int mx, int my){
+    public void newFriendlyBullet(int x, int y, int mx, int my){
         Bullet bullet = new Bullet(x,y,ID.Bullet, mediator,mx, my,BulletType.PLAYER,gameWidth,gameHeight);
         objectsContainer.addBullet(bullet);
-        return bullet;
     }
 
 
