95175b900cedc7b7fe58f95e9a6c13bf46c49a14
Balazs <Hungary3176>
2019-07-13 11:09:00 +0300

    Sounds

--- src/Model/SoundContainer.java
+++ src/Model/SoundContainer.java
@@ -7,6 +7,7 @@ import javax.sound.sampled.*;
 public enum SoundContainer {
     THEME("/res/theme_song.wav"),
     INJURED("/res/injured_sound.wav"),
+    EAT("/res/eat_sound.wav"),
     SQUISH("/res/squish_sound.wav"),
     PANDA1("/res/panda_sound_1.wav"),
     PANDA2("/res/panda_sound_2.wav"),
